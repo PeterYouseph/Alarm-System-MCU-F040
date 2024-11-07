f5e498a0e48693889b21388b3930b31e5cdfbc5b
PeterYouseph <jose_p@ufms.br>
2024-11-07 16:42:29 -0400

    feat: includes buzzer configuration

---
+++ buzzer.c
@@ -0,0 +1,16 @@
+#include "buzzer.h"
+#include "delay.h"
+
+#define BUZZER P2_7 // Pino do buzzer no MCU
+
+void buzzer()
+{
+    BUZZER = 1;
+    delay_ms(100);
+    BUZZER = 0;
+}
+
+void init_buzzer()
+{
+    BUZZER = 0; // Configura o pino do buzzer como saída inicial
+}
---
+++ buzzer.h
@@ -0,0 +1,7 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+void init_buzzer(void);
+void buzzer(void);
+
+#endif
