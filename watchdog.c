c10954003682fe2fb4b98b7897f2a62663d0a99f
José Pedro Cândido <61319056+PeterYouseph@users.noreply.github.com>
2024-11-12 10:44:49 -0400

    feat: implements Watchdog timer new base interrupt function

--- watchdog.c
+++ watchdog.c
@@ -1,5 +1,12 @@
 #include "watchdog.h"
 
+// Interruption para funcionamento do Watchdog Timer na F040
+
+void WDT_ISR(void) __interrupt 16
+{
+    WDTCN = 0xA5; // Reseta o watchdog timer
+}
+
 void init_watchdog()
 {
     WDTCN = 0xA5; // Ativa o watchdog timer
