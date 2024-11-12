6313d1da19fd05e3982b2895a13a5cc64460750c
José Pedro Cândido <61319056+PeterYouseph@users.noreply.github.com>
2024-11-12 10:46:28 -0400

    fix: remove glcd.c defines now defined in glcd.h

--- glcd.c
+++ glcd.c
@@ -1,29 +1,5 @@
-#define NOP4() \
-    NOP();     \
-    NOP();     \
-    NOP();     \
-    NOP()
-#define NOP8() \
-    NOP4();    \
-    NOP4()
-#define NOP12() \
-    NOP8();     \
-    NOP4()
-
-#define GLCD_ESQ 0
-#define GLCD_DIR 1
-#define GLCD_CO 0
-#define GLCD_DA 1
-#define GLCD_CS1 P2_0
-#define GLCD_CS2 P2_1
-#define GLCD_RST P2_5
-#define GLCD_RS P2_2
-#define GLCD_RW P2_3
-#define GLCD_ENABLE P2_4
-#define GLCD_DB P4
-#define LOW 0
-#define HIGH 1
-#define TECS_OPEN 255
+#include "glcd.h"
+#include "def_pinos.h"
 
 unsigned char le_glcd(__bit cd, __bit cs)
 {
